c3f65cc4a1c57ecf32b7d8c942daa6852573a8bc
Peter Huynh <peterhyh@yahoo.com>
2024-03-27 17:44:11 -0500

    User interface added

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-import psycopg2 as pg2
-import classified
+from tkinter import *
 
-conn = pg2.connect(database=classified.psql_database,
-                   user=classified.psql_username, password=classified.psql_password)
 
-cur = conn.cursor()
+def handleSubmit():
+    result = num_entry.get()
+    print(result)
 
-cur.execute(classified.psql_query)
 
-print(cur.fetchone())
+window = Tk()
+window.title('Data Science')
+window.geometry('650x250')
 
-conn.close()
+num_label = Label(text='Number: ')
+num_label.grid(column=0, row=0)
+
+num_entry = Entry()
+num_entry.grid(column=1, row=0)
+
+num_button = Button(text='Submit', command=handleSubmit)
+num_button.grid(column=1, row=1)
+
+window.mainloop()
+
+
+# import psycopg2 as pg2
+# import classified
+# conn = pg2.connect(database=classified.psql_database,
+#                    user=classified.psql_username, password=classified.psql_password)
+# cur = conn.cursor()
+# cur.execute(classified.psql_query)
+# print(cur.fetchone())
+# conn.close()
