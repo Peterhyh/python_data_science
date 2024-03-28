18bbbc3088b08e128abb8f1683393a64096c4b14
Peter Huynh <peterhyh@yahoo.com>
2024-03-27 19:44:51 -0500

    database login feature added

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 from tkinter import *
+import psycopg2 as pg2
 
 
-def handleSubmit():
-    result = num_entry.get()
-    print(result)
+def handleLogin():
+    database = database_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+    connectDatabase(database=database, password=password, username=username)
+
+
+def connectDatabase(database, username, password):
+    conn = pg2.connect(database=database,
+                       user=username, password=password)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM payment')
+    print(cur.fetchone())
+    conn.close()
 
 
 window = Tk()
+window.config(pady=50, padx=150)
 window.title('Data Science')
 window.geometry('650x250')
 
-num_label = Label(text='Number: ')
-num_label.grid(column=0, row=0)
+database_label = Label(text='Database: ')
+database_label.grid(column=0, row=1)
 
-num_entry = Entry()
-num_entry.grid(column=1, row=0)
+database_entry = Entry()
+database_entry.grid(column=1, row=1)
 
-num_button = Button(text='Submit', command=handleSubmit)
-num_button.grid(column=1, row=1)
+username_label = Label(text='Username: ')
+username_label.grid(column=0, row=2)
+
+username_entry = Entry()
+username_entry.grid(column=1, row=2)
+
+password_label = Label(text='Password: ')
+password_label.grid(column=0, row=3)
+
+password_entry = Entry()
+password_entry.grid(column=1, row=3)
+
+submit_button = Button(text='Submit', command=handleLogin)
+submit_button.grid(column=1, row=4)
 
 window.mainloop()
-
-
-# import psycopg2 as pg2
-# import classified
-# conn = pg2.connect(database=classified.psql_database,
-#                    user=classified.psql_username, password=classified.psql_password)
-# cur = conn.cursor()
-# cur.execute(classified.psql_query)
-# print(cur.fetchone())
-# conn.close()
