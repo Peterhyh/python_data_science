db1cf147305956672282bb1c7de0bef4e4b2bf74
Peter Huynh <peterhyh@yahoo.com>
2024-03-27 20:57:43 -0500

    querying SQL feature added

--- main.py
+++ main.py
@@ -6,16 +6,40 @@ def handleLogin():
     database = database_entry.get()
     username = username_entry.get()
     password = password_entry.get()
-    connectDatabase(database=database, password=password, username=username)
+    query = query_entry.get()
+    connectDatabase(database=database, password=password,
+                    username=username, query=query)
+    database_label.grid_forget()
+    database_entry.grid_forget()
+    username_label.grid_forget()
+    username_entry.grid_forget()
+    password_label.grid_forget()
+    password_entry.grid_forget()
+    login_button.grid_forget()
+    query_label.grid(column=0, row=1)
+    query_entry.grid(column=1, row=1)
+    query_button.grid(column=1, row=2)
 
 
-def connectDatabase(database, username, password):
+def connectDatabase(database, username, password, query):
     conn = pg2.connect(database=database,
                        user=username, password=password)
     cur = conn.cursor()
-    cur.execute('SELECT * FROM payment')
-    print(cur.fetchone())
+    if query:
+        cur.execute(query)
+        query_array = cur.fetchall()
+        for item in query_array:
+            print(item)
     conn.close()
+
+
+def handleQuery():
+    database = database_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+    query = query_entry.get()
+    connectDatabase(database=database, password=password,
+                    username=username, query=query)
 
 
 window = Tk()
@@ -41,7 +65,19 @@ password_label.grid(column=0, row=3)
 password_entry = Entry()
 password_entry.grid(column=1, row=3)
 
-submit_button = Button(text='Submit', command=handleLogin)
-submit_button.grid(column=1, row=4)
+login_button = Button(text='Login', command=handleLogin)
+login_button.grid(column=1, row=4)
+
+
+# After login:
+query_label = Label(text='Query: ')
+query_label.grid_forget()
+
+query_entry = Entry()
+query_entry.grid_forget()
+
+query_button = Button(text='Query', command=handleQuery)
+query_button.grid_forget()
+
 
 window.mainloop()
